f57b0c72554555e9194f4294d9ae0f79b731c1b3
Justin Morris <justin@pixelbloom.com>
2011-10-05 12:18:41 +1100

    Beh?

--- conf/sentinel/gunicorn.conf.py
+++ conf/sentinel/gunicorn.conf.py
@@ -1,5 +1,6 @@
 name = 'gunicorn_cobracommander'
 bind = '127.0.0.1:29001'
-logfile = '/Users/justin/Sites/_virtualenvs/cobracommander/logs/gunicorn.log'
+accesslog = '/Users/justin/Sites/_virtualenvs/cobracommander/logs/gunicorn_access.log'
+errorlog = '/Users/justin/Sites/_virtualenvs/cobracommander/logs/gunicorn_errors.log'
 workers = 4
 user = 'justin'
