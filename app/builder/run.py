6422b10da5c9bd9be64e4fa263008f90f8b762ce
Justin Morris <justin@pixelbloom.com>
2011-10-09 16:42:04 +1100

    Added django settings to run

--- app/builder/run.py
+++ app/builder/run.py
@@ -3,5 +3,13 @@
 # gunicorn -k "gevent" -b 0.0.0.0:9991 app.builder.run
 
 from .server import BuilderServer
+import settings
+import django.core.handlers.wsgi
+
+from django.core.management import setup_environ
+setup_environ(settings)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 server = BuilderServer()
 server.serve()
