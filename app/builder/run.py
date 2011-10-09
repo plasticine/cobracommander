b29ff820e04605c11f92c4d29436640eb13267d4
Justin Morris <justin@pixelbloom.com>
2011-10-09 16:57:37 +1100

    Add paths.

--- app/builder/run.py
+++ app/builder/run.py
@@ -3,14 +3,17 @@
 # gunicorn -k "gevent" -b 0.0.0.0:9991 app.builder.run
 import os, sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from gevent import monkey
 monkey.patch_all()
 
-import settings
 from django.core.management import setup_environ
+from cobracommander import settings
 setup_environ(settings)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cobracommander.settings'
 
-from .server import BuilderServer
+from server import BuilderServer
 server = BuilderServer()
 server.serve()
