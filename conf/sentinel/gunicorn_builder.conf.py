83c45676bc9fe4621b049e6ffc01acf8c3f09dd0
Justin Morris <justin@pixelbloom.com>
2011-10-09 18:21:17 +1100

    Conf files.

--- conf/sentinel/gunicorn_builder.conf.py
+++ conf/sentinel/gunicorn_builder.conf.py
@@ -16,6 +16,6 @@ bind = '0.0.0.0:9991'
 accesslog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_builder_access.log')
 errorlog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_builder_errors.log')
 pid = os.path.join(ensure_exists(virtualenv_dir, 'tmp', 'pids'), 'gunicorn_builder.pid')
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 user = 'justin'
 worker_class = 'gevent'
