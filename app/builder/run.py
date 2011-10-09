ee828131541ab4fe89ee60c82402af93ccc61502
Justin Morris <justin@pixelbloom.com>
2011-10-09 16:43:33 +1100

    Fix missing imports in run

--- app/builder/run.py
+++ app/builder/run.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 # gunicorn -k "gevent" -b 0.0.0.0:9991 app.builder.run
+import os, sys
 
-from .server import BuilderServer
+from gevent import monkey
+monkey.patch_all()
+
 import settings
-import django.core.handlers.wsgi
-
 from django.core.management import setup_environ
 setup_environ(settings)
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+from .server import BuilderServer
 server = BuilderServer()
 server.serve()
