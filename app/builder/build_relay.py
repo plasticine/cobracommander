57fd816e340d18e248e59bf344d8ca4ff7f545a1
Justin Morris <justin@pixelbloom.com>
2011-10-09 12:52:25 +1100

    status view -> queue view

--- app/builder/build_relay.py
+++ app/builder/build_relay.py
@@ -11,7 +11,7 @@ from .utils import get_logger
 from .utils.wsgi import WSGIWebsocketBase
 
 from .builder import Builder
-from .views import status, build
+from .views import queue, build
 
 class BuildRelay(WSGIWebsocketBase):
     """
@@ -22,7 +22,7 @@ class BuildRelay(WSGIWebsocketBase):
         self.builder = Builder()
         self.url_map = Map([
             Rule('/builder/status', endpoint=status.BuilderStatus(builder=self.builder)),
-            Rule('/builder/<build_id>/console', endpoint=build.BuildConsole()),
-            Rule('/builder/<build_id>/stop', endpoint=build.stop)
+            Rule('/builder/build/<build_id>/console', endpoint=build.BuildConsole()),
+            Rule('/builder/build/<build_id>/stop', endpoint=build.stop)
         ])
         super(BuildRelay, self).__init__()
