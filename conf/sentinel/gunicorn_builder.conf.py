52af1aae2f4093b84e613726db3450b45a075542
Justin Morris <justin@pixelbloom.com>
2011-10-09 15:56:03 +1100

    conf bs.

--- conf/sentinel/gunicorn_builder.conf.py
+++ conf/sentinel/gunicorn_builder.conf.py
@@ -11,11 +11,11 @@ virtualenv_dir = '/Users/justin/Sites/_virtualenvs/cobracommander'
 ensure_exists(virtualenv_dir, 'logs')
 ensure_exists(virtualenv_dir, 'tmp', 'pids')
 
-name = 'gunicorn_cobracommander'
-bind = '127.0.0.1:29001'
-accesslog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_access.log')
-errorlog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_errors.log')
-pid = os.path.join(ensure_exists(virtualenv_dir, 'tmp', 'pids'), 'gunicorn.pid')
+name = 'gunicorn_cobracommander_builder'
+bind = '127.0.0.1:9991'
+accesslog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_builder_access.log')
+errorlog = os.path.join(ensure_exists(virtualenv_dir, 'logs'), 'gunicorn_builder_errors.log')
+pid = os.path.join(ensure_exists(virtualenv_dir, 'tmp', 'pids'), 'gunicorn_builder.pid')
 workers = multiprocessing.cpu_count() * 2 + 1
 user = 'justin'
-worker_class = 'sync'
+worker_class = 'gevent'
