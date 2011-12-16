d2178f1d2ab91abaf91f0a68450964a7b1a29e0a
Justin Morris <justin@pixelbloom.com>
2011-12-16 19:08:29 +1100

    Added github build url

--- apps/build/urls.py
+++ apps/build/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    url(r'^project/(?P<name_slug>[0-9A-Za-z-]+)/(?P<refspec>[0-9A-Za-z-]+)/build/$',
-          views.target.build_target, name='build_target'),
-    url(r'^project/(?P<name_slug>[0-9A-Za-z-]+)/(?P<refspec>[0-9A-Za-z-]+)/build/(?P<id>[0-9]+)/$',
-          views.build.show, name='show'),
+  url(r'^project/(?P<name_slug>[0-9A-Za-z-]+)/build/$',
+        views.target.build_target, name='build_target'),
+  url(r'^project/(?P<name_slug>[0-9A-Za-z-]+)/(?P<refspec>[0-9A-Za-z-]+)/build/$',
+        views.target.build_target, name='build_target'),
+  url(r'^project/(?P<name_slug>[0-9A-Za-z-]+)/(?P<refspec>[0-9A-Za-z-]+)/build/(?P<id>[0-9]+)/$',
+        views.build.show, name='show'),
 )
--- apps/build/views/target.py
+++ apps/build/views/target.py
@@ -9,18 +9,26 @@ from ..models.target import Target
 from ..models.build import Build
 from apps.project.models import Project
 
-def build_target(request, name_slug, refspec):
+def build_target(request, name_slug, refspec=None):
     """
     Trigger a build run on the given target and project.
 
     Adds the build details to the redis queue for minion to pick up and process.
     Creates a new Build instance and creates the relationship with the Target.
+
+    If `refspec` is passed then the build was probably started by cobracommander.
+    If it is not present it usually means that it was triggered by a GitHub
+    post-commit hook.
     """
-
-    project = get_object_or_404(Project, name_slug=name_slug)
-    target = get_object_or_404(Target, project=project, refspec=refspec)
-
     if request.method == 'POST':
+      project = get_object_or_404(Project, name_slug=name_slug)
+
+      if not refspec:
+        import simplejson as json
+        github_postcommit_data = json.loads(request.POST)
+        refspec = github_postcommit_data['ref'].split('/')[-1]
+
+      target = get_object_or_404(Target, project=project, refspec=refspec)
       build = Build(project=project)
       build.save()
       target.builds.add(build)
