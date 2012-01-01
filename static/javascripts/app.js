86ab79984a132fa30ca70b06206f04bfdfda6d34
Justin Morris <justin@pixelbloom.com>
2012-01-02 09:18:50 +1100

    Remove garbage js files.

--- static/javascripts/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  require(['lib/module', 'lib/Template', 'app/build_queue'], function() {
-    return this.module("cc", function() {
-      return cc.version = "0.1";
-    });
-  });
-}).call(this);
